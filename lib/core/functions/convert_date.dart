b7be76b61e319153c381db634db30c1d8ab99c15
M Wahyu Ayesha <mine215567@gmail.com>
2025-08-25 16:38:58 +0700

    Fix tanggal pembuatan akun

--- lib/core/functions/convert_date.dart
+++ lib/core/functions/convert_date.dart
@@ -1,17 +1,13 @@
-import 'package:intl/intl.dart';
-
-/// parsing tanggal dari API HTTP date format
-DateTime parseHttpDate(String tglStr) {
-  try {
-    return DateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", 'en_US')
-        .parseUtc(tglStr)
-        .toLocal();
-  } catch (_) {
-    return DateTime.now(); // fallback kalau format salah
-  }
-}
-
-/// format DateTime ke "20 Des 2025"
 String formatTanggal(DateTime tgl) {
-  return DateFormat('d MMM yyyy', 'id_ID').format(tgl);
+  const namaBulan = [
+    '', // supaya index 1 = januari
+    'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun',
+    'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'
+  ];
+
+  final hari = tgl.day;
+  final bulan = namaBulan[tgl.month];
+  final tahun = tgl.year;
+
+  return '$hari $bulan $tahun';
 }
