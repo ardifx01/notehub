6779a5859957f0237f5daa42fea04ff4db5f26f9
FajarFarel <farelparjo@gmail.com>
2025-08-25 10:14:57 +0700

    backend_2

--- backend_notehub/run.py
+++ backend_notehub/run.py
@@ -177,4 +177,5 @@ def get_saved_notes(user_id):
 # RUN APP
 # ======================
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Binding ke semua IP (biar bisa diakses dari jaringan lokal)
+    app.run(host="0.0.0.0", port=5000, debug=True)
