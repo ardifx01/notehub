b3db1904e6eaca5d3c79509a240873285a249181
FajarFarel <farelparjo@gmail.com>
2025-09-09 09:10:27 +0700

    frontend

--- backend_notehub/config.py
+++ backend_notehub/config.py
@@ -2,7 +2,7 @@ DB_HOST = "10.0.5.41"
 DB_USER = "fajar"
 DB_PASSWORD = "Nova0920"
 DB_NAME = "notehub"
-Base_URL = "https://14486fc5c21f.ngrok-free.app"
+Base_URL = "https://0b73f882b8ce.ngrok-free.app"
 # Base_URL = "http://10.0.5.41:5000"
 # Base_URL = "http://192.168.1.103:5000"
 
