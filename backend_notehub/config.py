5db33f74274f2e0eaefced27f645b14fdf9f634a
FajarFarel <farelparjo@gmail.com>
2025-09-12 13:09:49 +0700

    flutter baik

--- backend_notehub/config.py
+++ backend_notehub/config.py
@@ -2,7 +2,7 @@ DB_HOST = "10.0.5.41"
 DB_USER = "fajar"
 DB_PASSWORD = "Nova0920"
 DB_NAME = "notehub"
-Base_URL = " https://6514cb44b614.ngrok-free.app"
+Base_URL = "https://f6f9ac487002.ngrok-free.app"
 # Base_URL = "http://10.0.5.41:5000"
 # Base_URL = "http://192.168.1.103:5000"
 
